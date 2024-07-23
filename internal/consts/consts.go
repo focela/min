391d25ffe1ace9789dd1dd66604a34e5d323b584
Son Tran Thanh <286.trants@gmail.com>
2024-07-23 11:27:34 +0700

    feat(consts): provide shared constants * Package consts defines constants that are shared all among packages of framework.

---
+++ internal/consts/consts.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+// Package consts defines constants that are shared all among packages of framework.
+package consts
+
+const (
+	ConfigNodeNameDatabase        = "database"
+	ConfigNodeNameLogger          = "logger"
+	ConfigNodeNameRedis           = "redis"
+	ConfigNodeNameViewer          = "viewer"
+	ConfigNodeNameServer          = "server"
+	ConfigNodeNameServerSecondary = "httpserver"
+	StackFilterKeyForRatcatcher   = "github.com/focela/ratcatcher/"
+)
