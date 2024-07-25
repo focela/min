7e36874567c1b9d9aef002aa4f321f2ed4348693
Son Tran Thanh <286.trants@gmail.com>
2024-07-25 13:12:32 +0700

    feat(rcvar): statement declares a list of variables * Package rcvar provides an universal variable type, like generics.

---
+++ container/rcvar/is.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package rcvar
+
+import "github.com/focela/ratcatcher/internal/utils"
+
+// IsNil checks whether `v` is nil.
+func (v *Var) IsNil() bool {
+	return utils.IsNil(v.Val())
+}
+
+// IsEmpty checks whether `v` is empty.
+func (v *Var) IsEmpty() bool {
+	return utils.IsEmpty(v.Val())
+}
+
+// IsInt checks whether `v` is type of int.
+func (v *Var) IsInt() bool {
+	return utils.IsInt(v.Val())
+}
+
+// IsUint checks whether `v` is type of uint.
+func (v *Var) IsUint() bool {
+	return utils.IsUint(v.Val())
+}
+
+// IsFloat checks whether `v` is type of float.
+func (v *Var) IsFloat() bool {
+	return utils.IsFloat(v.Val())
+}
+
+// IsSlice checks whether `v` is type of slice.
+func (v *Var) IsSlice() bool {
+	return utils.IsSlice(v.Val())
+}
+
+// IsMap checks whether `v` is type of map.
+func (v *Var) IsMap() bool {
+	return utils.IsMap(v.Val())
+}
+
+// IsStruct checks whether `v` is type of struct.
+func (v *Var) IsStruct() bool {
+	return utils.IsStruct(v.Val())
+}
