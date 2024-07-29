2aaca314cefd17b78af48541e98ccecc976f9b1c
Son Tran Thanh <286.trants@gmail.com>
2024-07-29 15:20:06 +0700

    feat(utils): some utility functions for internal usage - Package utils provides some utility functions for internal usage.

---
+++ internal/utils/debug.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package utils
+
+import "github.com/focela/ratcatcher/internal/cmd"
+
+const (
+	// Debug key for checking if in debug mode.
+	commandEnvKeyForDebugKey = "r.debug"
+)
+
+var (
+	// isDebugEnabled marks whether Ratcatcher debug mode is enabled.
+	isDebugEnabled = false
+)
+
+func init() {
+	// Debugging configured.
+	value := cmd.GetOptWithEnv(commandEnvKeyForDebugKey)
+	if value == "" || value == "0" || value == "false" {
+		isDebugEnabled = false
+	} else {
+		isDebugEnabled = true
+	}
+}
+
+// IsDebugEnabled checks and returns whether debug mode is enabled.
+// The debug mode is enabled when command argument "r.debug" or environment "R_DEBUG" is passed.
+func IsDebugEnabled() bool {
+	return isDebugEnabled
+}
+
+// SetDebugEnabled enables/disables the internal debug info.
+func SetDebugEnabled(enabled bool) {
+	isDebugEnabled = enabled
+}
---
+++ internal/utils/map.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package utils
+
+// MapPossibleItemByKey tries to find the possible key-value pair for given key ignoring cases and symbols.
+//
+// Note that this function might be of low performance.
+func MapPossibleItemByKey(data map[string]interface{}, key string) (foundKey string, foundValue interface{}) {
+	if len(data) == 0 {
+		return
+	}
+	if v, ok := data[key]; ok {
+		return key, v
+	}
+	// Loop checking.
+	for k, v := range data {
+		if EqualFoldWithoutChars(k, key) {
+			return k, v
+		}
+	}
+	return "", nil
+}
+
+// MapContainsPossibleKey checks if the given `key` is contained in given map `data`.
+// It checks the key ignoring cases and symbols.
+//
+// Note that this function might be of low performance.
+func MapContainsPossibleKey(data map[string]interface{}, key string) bool {
+	if k, _ := MapPossibleItemByKey(data, key); k != "" {
+		return true
+	}
+	return false
+}
