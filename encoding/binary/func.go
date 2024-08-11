689e1b6686ca9905c710ca0c99413063bbf18648
Son Tran Thanh <286.trants@gmail.com>
2024-08-12 01:31:55 +0700

    Initial commit

--- encoding/binary/func.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2024 Focela Technologies. All rights reserved.
-// Use of this source code is governed by an MIT style
-// license that can be found in the LICENSE file.
-
-package binary
--- internal/utils/utils.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2024 Focela Technologies. All rights reserved.
-// Use of this source code is governed by an MIT style
-// license that can be found in the LICENSE file.
-
-// Package util provides some utility functions for internal use.
-package utils
