a148ff89352e7b6d36727e3a37ff11fc8226753f
Son Tran Thanh <286.trants@gmail.com>
2024-08-18 00:43:32 +0700

    Add package for universal error code definitions
    - Introduce `Code` interface for handling error codes
    - Implement common error codes like `CodeOK`, `CodeInternalError`, etc.
    - Add `New` and `WithCode` functions to create and manipulate error codes
    - Provide comments and documentation explaining each code's purpose

--- errors/code/local.go
+++ errors/code/local.go
@@ -5,32 +5,33 @@
 
 package code
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// localCode is an implementation of the Code interface for internal use only.
+// localCode implements the Code interface and is intended for internal use only.
 type localCode struct {
 	code    int         // Error code, usually an integer.
-	message string      // Brief message associated with this error code.
-	detail  interface{} // Detailed information, mainly designed as an extension field for the error code.
+	message string      // Brief description of the error.
+	detail  interface{} // Optional field for additional error information.
 }
 
-// Code returns the integer value of the current error code.
+// Code returns the integer value of the error code.
 func (c localCode) Code() int {
 	return c.code
 }
 
-// Message returns the brief message associated with the current error code.
+// Message returns a brief description of the error.
 func (c localCode) Message() string {
 	return c.message
 }
 
-// Detail returns the detailed information of the current error code,
-// which is mainly designed as an extension field for the error code.
+// Detail provides additional information for the error code.
 func (c localCode) Detail() interface{} {
 	return c.detail
 }
 
-// String returns the current error code as a string.
+// String converts the error code to a string representation.
 func (c localCode) String() string {
 	if c.detail != nil {
 		return fmt.Sprintf(`%d:%s %v`, c.code, c.message, c.detail)
