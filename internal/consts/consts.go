32c77394335018cf62575aaba84018c77c1365a6
Son Tran Thanh <286.trants@gmail.com>
2024-08-18 18:31:15 +0700

    Add consts package with shared constants for Min - Define constants for configuration node names like database, logger, etc. - Update the stack filter key to reflect the Min project

---
+++ internal/consts/consts.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+//
+// Use of this source code is governed by an MIT-style license
+// that can be found in the LICENSE file.
+
+// Package consts defines constants shared across all packages in the framework.
+package consts
+
+const (
+	ConfigNodeNameDatabase        = "database"
+	ConfigNodeNameLogger          = "logger"
+	ConfigNodeNameRedis           = "redis"
+	ConfigNodeNameViewer          = "viewer"
+	ConfigNodeNameServer          = "server"
+	ConfigNodeNameServerSecondary = "httpserver"
+	StackFilterKeyForMin          = "github.com/focela/min/"
+)
