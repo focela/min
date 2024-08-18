5a571c06e7b0dc99450b288fef11517f3bef847e
Son Tran Thanh <286.trants@gmail.com>
2024-08-18 23:37:28 +0700

    Initial commit

--- encoding/hash/hash.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2024 Focela Technologies. All rights reserved.
-//
-// Use of this source code is governed by an MIT-style license
-// that can be found in the LICENSE file.
-
-// Package hash provides some classic hash functions (uint32 and uint64) in Go.
-package hash
