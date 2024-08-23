61dea48d43cbdf602a9df471871c3727bd64276d
Son Tran Thanh <286.trants@gmail.com>
2024-08-24 02:30:27 +0700

    feat(consts): add config node names and filter keys
    - Define constants for common configuration nodes such as database, logger, redis, and server.
    - Include a filter key specific to the Min framework for stack filtering.

---
+++ internal/consts/consts.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+//
+// Use of this source code is governed by an MIT-style license
+// that can be found in the LICENSE file.
+
+// Package consts defines shared configuration node names and filter keys for the Min framework.
+package consts
+
+const (
+	NodeDatabase        = "database"
+	NodeLogger          = "logger"
+	NodeRedis           = "redis"
+	NodeViewer          = "viewer"
+	NodeServer          = "server"
+	NodeServerSecondary = "httpserver"
+	MinStackFilterKey   = "github.com/focela/min/"
+)
